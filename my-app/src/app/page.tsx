2bfd1083cc43690ad32a764f90e1fab83e8b654f
Obua Stella <stellaobua123@gmail.com>
2025-02-26 19:11:31 +0100

    initialized repo and installed necessary libraries

---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-red-400 text-2xl">Hello World</h1>
+      <Button>Shadcn button</Button>
+    </>
+  );
+}
